048aecabab8fd10fd4920924f617b579402ab848
alvaroEspinozaMerida <111097688+alvaroEspinozaMerida@users.noreply.github.com>
2024-03-28 12:15:19 -0700

    added FCFS and SJF implementation still need to add comments

--- assign3/driver_fcfs.cpp
+++ assign3/driver_fcfs.cpp
@@ -15,8 +15,8 @@ using namespace std;
 
 int main(int argc, char *argv[]) {
     std::cout << "CS 433 Programming assignment 3" << std::endl;
-    std::cout << "Author: xxxxxx and xxxxxxx" << std::endl;     // TODO: add your name
-    std::cout << "Date: xx/xx/20xx" << std::endl;               // TODO: add date
+    std::cout << "Author: Rutillo Maciel and Alvaro Espinoza" << std::endl;
+    std::cout << "Date: 3/28/2024" << std::endl;
     std::cout << "Course: CS433 (Operating Systems)" << std::endl;
     std::cout << "Description : test FCFS scheduling algorithm " << std::endl;
     std::cout << "=================================" << std::endl;
--- assign3/scheduler_fcfs.h
+++ assign3/scheduler_fcfs.h
@@ -17,7 +17,11 @@
  */
 class SchedulerFCFS : public Scheduler {
 private:
-    // TODO: add necessary member variables here for your implementation
+
+    vector<PCB> process_list;
+    double tTime;
+    double wTime;
+
 
 public:
     /**
